8641cc08bfa8cc0e42ba647b3e04c96914d3019b
wyswill <1151373571@qq.com>
2021-07-12 18:57:51 +0800

    feat:  添加部署脚本

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,13 @@
+const pkg = require('./package.json');
 module.exports = {
+  publicPath: `/${pkg.name}/`,
+  outputDir: `${pkg.buildName}/${pkg.name}`,
   css: {
-    loaderOptions: { // 向 CSS 相关的 loader 传递选项
+    loaderOptions: {
+      // 向 CSS 相关的 loader 传递选项
       less: {
-        javascriptEnabled: true
-      }
-    }
-  }
+        javascriptEnabled: true,
+      },
+    },
+  },
 };
