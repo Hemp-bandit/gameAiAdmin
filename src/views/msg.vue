4a31c293ba4f61dd97cdf9b505dd9156e8ce248f
wyswill <1151373571@qq.com>
2021-07-13 20:31:35 +0800

    fix

--- src/views/msg.vue
+++ src/views/msg.vue
@@ -1,98 +1,99 @@
 <template>
-  <div class="msg">
-    <div class="searchGroup">
-      <el-form :model="searchOpt" label-width="80px" size="mini" :inline="true">
-        <el-form-item label="文本内容">
-          <el-input v-model="searchOpt.content" />
+  <div class='msg'>
+    <div class='searchGroup'>
+      <el-form :model='searchOpt' label-width='80px' size='mini' :inline='true'>
+        <el-form-item label='文本内容'>
+          <el-input v-model='searchOpt.content' />
         </el-form-item>
-        <el-form-item label="文本类型">
-          <el-select v-model="searchOpt.wordType">
+        <el-form-item label='文本类型'>
+          <el-select v-model='searchOpt.wordType'>
             <el-option
-              v-for="(ele, index) in wordTypes"
-              :key="index"
-              :label="ele.name"
-              :value="ele.name"
+              v-for='(ele, index) in wordTypes'
+              :key='index'
+              :label='ele.name'
+              :value='ele.name'
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="命中类型">
-          <el-select v-model="searchOpt.checkType">
+        <el-form-item label='命中类型'>
+          <el-select v-model='searchOpt.checkType'>
             <el-option
-              v-for="(ele, index) in checkType"
-              :key="index"
-              :label="ele.name"
-              :value="ele.name"
+              v-for='(ele, index) in checkType'
+              :key='index'
+              :label='ele.name'
+              :value='ele.name'
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="时间范围">
+        <el-form-item label='时间范围'>
           <el-date-picker
-            v-model="searchOpt.date"
-            type="datetimerange"
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
+            v-model='searchOpt.date'
+            type='datetimerange'
+            range-separator='至'
+            start-placeholder='开始日期'
+            end-placeholder='结束日期'
           >
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="ip">
-          <vueIp :set_ip="searchOpt.ip" @get-ip="onIpChange" />
+        <el-form-item label='ip'>
+          <vueIp :set_ip='searchOpt.ip' @get-ip='onIpChange' />
         </el-form-item>
-        <el-form-item label="角色id">
-          <el-input v-model="searchOpt.gameUserId" />
+        <el-form-item label='角色id'>
+          <el-input v-model='searchOpt.gameUserId' />
         </el-form-item>
-        <el-form-item label="处理结果">
-          <el-select v-model="searchOpt.checkResult">
+        <el-form-item label='处理结果'>
+          <el-select v-model='searchOpt.checkResult'>
             <el-option
-              v-for="(ele, index) in checkResults"
-              :key="index"
-              :label="ele.name"
-              :value="ele.name"
+              v-for='(ele, index) in checkResults'
+              :key='index'
+              :label='ele.name'
+              :value='ele.name'
             ></el-option>
           </el-select>
         </el-form-item>
         <el-form-item>
-          <el-button type="info">重置</el-button>
-          <el-button type="primary">搜索</el-button>
+          <el-button type='info'>重置</el-button>
+          <el-button type='primary' @click='searchMsg'>搜索</el-button>
         </el-form-item>
       </el-form>
     </div>
-    <el-table :data="dataList" height="600">
-      <el-table-column prop="content" label="文本内容" />
-      <el-table-column label="文本类型">
-        <template #default="scoped">
-          {{ conveType(scoped.row.type, 'word') }}
+    <el-table :data='dataList' height='600'>
+      <el-table-column prop='content' label='文本内容' />
+      <el-table-column label='文本类型'>
+        <template #default='scoped'>
+          {{ conveType( scoped.row.type, 'word' ) }}
         </template>
       </el-table-column>
-      <el-table-column prop="ip" label="ip" />
-      <el-table-column prop="result" label="命中类型" />
-      <el-table-column prop="result" label="审核依据">
-        <template #default="scoped">
-          {{ conveType(scoped.row.checkType, 'ckt') }}
+      <el-table-column prop='ip' label='ip' />
+      <el-table-column prop='result' label='命中类型' />
+      <el-table-column prop='result' label='审核依据'>
+        <template #default='scoped'>
+          {{ conveType( scoped.row.checkType, 'ckt' ) }}
         </template>
       </el-table-column>
-      <el-table-column prop="gameUserId" label="角色id" />
-      <el-table-column label="处理结果">
-        <template #default="scoped">
-          {{ conveType(scoped.row.checkResult, 'chr') }}
+      <el-table-column prop='gameUserId' label='角色id' />
+      <el-table-column label='处理结果'>
+        <template #default='scoped'>
+          {{ conveType( scoped.row.checkResult, 'chr' ) }}
         </template>
       </el-table-column>
-      <el-table-column label="操作">
-        <template #default="scope">
+      <el-table-column label='操作'>
+        <template #default='scope'>
           <el-cascader
-            v-model="scope.row.opt"
-            :options="scope.row.opts"
+            v-model='scope.row.opt'
+            :options='scope.row.opts'
             :props="{
               checkStrictly: false,
               multiple: true,
               value: 'name',
               label: 'name',
             }"
-            :clearable="true"
-            @change="handChange(scope.row, scope.$index)"
+            :clearable='true'
+            @change='handChange(scope.row, scope.$index)'
           ></el-cascader>
-          <el-button type="primary" size="mini" @click="changeMsg(scope.row)"
-            >确认修改</el-button
+          <el-button type='primary' size='mini' @click='changeMsg(scope.row)'
+          >确认修改
+          </el-button
           >
         </template>
       </el-table-column>
@@ -100,125 +101,136 @@
   </div>
 </template>
 
-<script lang="ts">
-import { queryGameList, queryMsgList, updateMsg } from '@/utile/api';
-import { CHECKRESULT, CHECKTYPE, LISTTYPE, WORDTYPES } from '@/utile/cosnt';
-import { defineComponent } from '@vue/runtime-core';
-import { ElMessage } from 'element-plus';
+<script lang='ts'>
+import { queryGameList, queryMsgList, updateMsg }      from '@/utile/api'
+import { CHECKRESULT, CHECKTYPE, LISTTYPE, WORDTYPES } from '@/utile/cosnt'
+import { defineComponent }                             from '@vue/runtime-core'
+import { ElMessage }                                   from 'element-plus'
 //@ts-ignore
-import vueIp from 'ip-input-vue';
-import _ from 'lodash';
-export default defineComponent({
+import vueIp                                           from 'ip-input-vue'
+import _                                               from 'lodash'
+
+export default defineComponent( {
   components: { vueIp },
   data() {
     return {
-      searchOpt: {
-        content: '',
-        wordType: '',
-        date: '',
-        ip: '',
-        gameUserId: '',
-        checkType: '',
-        checkResult: '',
+      searchOpt   : {
+        content    : '',
+        wordType   : '',
+        date       : '',
+        ip         : '',
+        gameUserId : '',
+        checkType  : '',
+        checkResult: ''
       },
-      wordTypes: [{ name: '聊天', value: '0' }],
-      checkType: [
+      wordTypes   : [ { name: '聊天', value: '0' } ],
+      checkType   : [
         { name: 'ai', value: '0' },
         { name: '人工', value: '1' },
-        { name: '词库', value: '2' },
+        { name: '词库', value: '2' }
       ],
       checkResults: [
         { name: '成功', value: '0' },
-        { name: '失败', value: '1' },
+        { name: '失败', value: '1' }
       ],
-      dataList: [],
-      optRes: '',
-    };
+      dataList    : [],
+      optRes      : ''
+    }
   },
   mounted() {
-    this.queryData().then(() => {
-      this.queryWord();
-    });
+    this.queryData()
+      .then( () => {
+        this.queryWord()
+      } )
   },
   methods: {
-    onIpChange(ip: string) {
-      this.searchOpt.ip = ip;
+    onIpChange( ip: string ) {
+      this.searchOpt.ip = ip
     },
-    async queryData(req: any = {}) {
+    async queryData( req: any = {} ) {
       try {
-        const { data } = await queryMsgList(req);
-        this.dataList = data;
-      } catch (error) {
-        ElMessage.error(error.message);
+        const { data } = await queryMsgList( req )
+        this.dataList = data
+      } catch ( error ) {
+        ElMessage.error( error.message )
       }
     },
-    conveType(row: string, tp: 'word' | 'ckt' | 'chr') {
-      switch (tp) {
+    conveType( row: string, tp: 'word' | 'ckt' | 'chr' ) {
+      switch ( tp ) {
         case 'word':
-          return WORDTYPES[Number(row)];
+          return WORDTYPES[ Number( row ) ]
         case 'ckt':
-          return CHECKTYPE[Number(row)];
+          return CHECKTYPE[ Number( row ) ]
         case 'chr':
-          return CHECKRESULT[Number(row)];
+          return CHECKRESULT[ Number( row ) ]
       }
     },
     async queryWord() {
       try {
-        let { data } = await queryGameList(LISTTYPE.word);
-        const opt = [{ name: '通过' }].concat(data);
-        this.dataList = this.dataList.map(ele => {
-          Reflect.set(ele, 'opts', opt);
-          return ele;
-        });
-      } catch (error) {
-        ElMessage.error(error.message);
+        let { data } = await queryGameList( LISTTYPE.word )
+        const opt = [ { name: '通过' } ].concat( data )
+        this.dataList = this.dataList.map( ele => {
+          Reflect.set( ele, 'opts', opt )
+          return ele
+        } )
+      } catch ( error ) {
+        ElMessage.error( error.message )
       }
     },
-    handChange({ opt, opts }: any, index: number) {
-      const fltOpt = _.flatten(opt);
-      if (fltOpt.includes('通过')) {
+    handChange( { opt, opts }: any, index: number ) {
+      const fltOpt = _.flatten( opt )
+      if ( fltOpt.includes( '通过' ) ) {
         //@ts-ignore
-        this.dataList[index].opt = [['通过']];
-        let i = 1;
-        while (i < opts.length) {
-          Reflect.set(opts[i], 'disabled', true);
-          i++;
+        this.dataList[ index ].opt = [ [ '通过' ] ]
+        let i = 1
+        while ( i < opts.length ) {
+          Reflect.set( opts[ i ], 'disabled', true )
+          i++
         }
-      } else {
-        let i = 1;
-        while (i < opts.length) {
-          Reflect.set(opts[i], 'disabled', false);
-          i++;
+      }
+      else {
+        let i = 1
+        while ( i < opts.length ) {
+          Reflect.set( opts[ i ], 'disabled', false )
+          i++
         }
       }
     },
-    async changeMsg(scop: any) {
-      const { opt } = scop;
-      const fltOpt = _.flatten(opt);
-      Reflect.deleteProperty(scop, 'opt');
-      Reflect.deleteProperty(scop, 'opts');
-      if (fltOpt.includes('通过')) {
-        scop.checkType = '1';
-        scop.checkResult = '0';
-        scop.result = `人工审核通过`;
-      } else {
-        scop.checkType = '2';
-        scop.checkResult = '1';
-        scop.result = `词库 ${fltOpt.join(' | ')} 审核不通过`;
+    async changeMsg( scop: any ) {
+      const { opt } = scop
+      const fltOpt = _.flatten( opt )
+      Reflect.deleteProperty( scop, 'opt' )
+      Reflect.deleteProperty( scop, 'opts' )
+      if ( fltOpt.includes( '通过' ) ) {
+        scop.checkType = '1'
+        scop.checkResult = '0'
+        scop.result = `人工审核通过`
+      }
+      else {
+        scop.checkType = '2'
+        scop.checkResult = '1'
+        scop.result = `词库 ${ fltOpt.join( ' | ' ) } 审核不通过`
       }
 
       // console.log(JSON.stringify(scop, null, 2));
 
       try {
-        const { msg } = await updateMsg(scop);
-        ElMessage.success(msg);
-        await this.queryData();
-      } catch (error) {
-        ElMessage.error(error.message);
+        const { msg } = await updateMsg( scop )
+        ElMessage.success( msg )
+        await this.queryData()
+      } catch ( error ) {
+        ElMessage.error( error.message )
       }
     },
-  },
-});
+    async searchMsg() {
+      try {
+        const {} = this
+      } catch ( e ) {
+
+      }
+    }
+  }
+} )
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang='less'></style>
